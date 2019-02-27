3477b4905137b5a09a95719419ba73a67fa27dcf
unknown <kolpak.conrad@gmail.com>
2019-02-27 20:43:36 +0000

    model scaling, physics

--- src/Game.js
+++ src/Game.js
@@ -18,21 +18,13 @@ export default class Game {
   }
 
   createScene() {
-    // Create a basic BJS Scene object.
     this.scene = new BABYLON.Scene(this.engine);
-    //this.scene.enablePhysics();
-   // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-
     // var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 200, -20), this.scene);
     // camera.attachControl(this.canvas, true);
-
     var light0 = new BABYLON.DirectionalLight("Omni", new BABYLON.Vector3(-2, -5, 2), this.scene);
-
     //Create a basic light, aiming 0,1,0 - meaning, to the sky.
     this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this.scene);
-
     this.scene.actionManager = new BABYLON.ActionManager(this.scene);
-
     this.scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger,  (evt) => {
       this.map[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
     }));
@@ -47,94 +39,59 @@ export default class Game {
       })
     })
 
-
-
-    var camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0,0,0), this.scene);
-
-// The goal distance of camera from target
-    camera.radius = 5;
-
-// The goal height of camera above local origin (centre) of target
-    camera.heightOffset = -0.5;
-
-// The goal rotation of camera around local origin (centre) of target in x y plane
+    let camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0,0,0), this.scene);
+    camera.radius = 45;
+    camera.heightOffset = 5;
     camera.rotationOffset = 0;
-
-
-// The speed at which acceleration is halted
     camera.maxCameraSpeed = 10
-
-// This attaches the camera to the canvas
     camera.attachControl(this.canvas, true);
 
-    // this.player.render(this.scene).then((mesh) => {
-    //   console.log('player added')
-    //   camera.lockedTarget = this.player.mesh;
-    //   this.loaded = true
-    // })
-    //
-    BABYLON.SceneLoader.ImportMesh(null, "/public/models/male_adventurer/", "Scene.gltf", this.scene, (meshes, particleSystems, skeletons) => {
-          this.player.mesh = meshes[0]
-          //this.player.mesh.scaling.multiply(new BABYLON.Vector3(50, 50, 50))
-          this.player.mesh.position.y = 100
-          camera.lockedTarget = this.player.mesh;
-          this.playerAnimations = this.scene.animationGroups
-          this.getAnimationByName("idle").play(true)
+    this.player.render(this.scene).then((mesh) => {
+      console.log('player added')
+      camera.lockedTarget = this.player.mesh;
+      this.playerAnimations = this.scene.animationGroups
+      this.getAnimationByName("idle").play(true)
+      this.loaded = true
     })
-
-    this.scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
     this.scene.collisionsEnabled = true;
-    //let music = new BABYLON.Sound("Music", "/public/sounds/nature_theme1.mp3", this.scene, null, { loop: true, autoplay: true });
-
-
-      this.scene.registerAfterRender( () => {
-        //let directionVector = direction.subtract(this.player.mesh.position);
-
-        if (this.player.mesh) {
-          if ((this.map["w"] || this.map["W"])) {
-
-            var rotation = this.player.mesh.rotation;
-            if(this.player.mesh.rotationQuaternion){
-              rotation = this.player.mesh.rotationQuaternion.toEulerAngles();
-            }
-            rotation.negate();
-            var forwards = new BABYLON.Vector3(-parseFloat(Math.sin(rotation.y)) / 1, 0, -parseFloat(Math.cos(rotation.y)) / 1);
-            this.player.mesh.moveWithCollisions(forwards)
-
-            this.getAnimationByName("run").play(true)
-
-            //this.player.mesh.translate(new BABYLON.Vector3(0, 0, -1), 2, BABYLON.Space.LOCAL);
-            // this.player.mesh.position.z += speed
-          } else {
-            this.getAnimationByName("run").stop()
+    let music = new BABYLON.Sound("Music", "/public/sounds/nature_theme1.mp3", this.scene, null, { loop: true, autoplay: true });
+    this.scene.registerAfterRender( () => {
+      if (this.player.mesh) {
+        if ((this.map["w"] || this.map["W"])) {
+          let rotation = this.player.mesh.rotation;
+          if(this.player.mesh.rotationQuaternion){
+            rotation = this.player.mesh.rotationQuaternion.toEulerAngles();
           }
-          // if ((this.map[" "])) {
-          //   console.log(this.player.mesh.position)
-          //   this.player.mesh.position.y -= -2
-          // };
-          if ((this.map["s"] || this.map["s"])) {
-            //this.player.mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0,0,5))
-            //this.player.mesh.moveWithCollisions(new BABYLON.Vector3(0, 0, 10))
-          };
-          if ((this.map["d"] || this.map["D"])) {
-            this.player.mesh.rotate(BABYLON.Axis.Y,0.01, BABYLON.Space.WORLD);
-          };
-
-          if ((this.map["a"] || this.map["A"])) {
-            this.player.mesh.rotate(BABYLON.Axis.Y,-0.01, BABYLON.Space.WORLD);
-          };
-
-          this.player.mesh.moveWithCollisions( new BABYLON.Vector3(0, -0.9, 0));
+          rotation.negate();
+          this.player.mesh.moveWithCollisions(new BABYLON.Vector3(-parseFloat(Math.sin(rotation.y)) / 1, 0, -parseFloat(Math.cos(rotation.y)) / 1))
+          this.getAnimationByName("run").play(true)
+        } else {
+          this.getAnimationByName("run").stop()
         }
-      });
+        // if ((this.map[" "])) {
+        //   this.player.mesh.position.y += 2
+        // };
+        if ((this.map["s"] || this.map["s"])) {
+          //this.player.mesh.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0,0,5))
+          //this.player.mesh.moveWithCollisions(new BABYLON.Vector3(0, 0, 10))
+        };
+        if ((this.map["d"] || this.map["D"])) {
+          this.player.mesh.rotate(BABYLON.Axis.Y,0.01, BABYLON.Space.WORLD);
+        };
+
+        if ((this.map["a"] || this.map["A"])) {
+          this.player.mesh.rotate(BABYLON.Axis.Y,-0.01, BABYLON.Space.WORLD)
+        };
+
+        this.player.mesh.moveWithCollisions(this.scene.gravity);
+      }
+    });
   }
 
   getAnimationByName(name) {
-    console.log(name)
     let animation = null
     this.playerAnimations.forEach((anim) => {
       if (anim.name === name) {
-        console.log(anim, 'tutaj')
         animation = anim
       }
     })
--- src/Player.js
+++ src/Player.js
@@ -6,54 +6,25 @@ export default class Player {
 
   }
 
-  // render(scene) {
-  //   this.mesh = BABYLON.MeshBuilder.CreateSphere("mySphere", {diameter: 3}, scene);
-  //
-  //   let material = new BABYLON.StandardMaterial('playerMaterial', scene);
-  //   material.alpha = 1;
-  //   material.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
-  //   this.mesh.material = material;
-  //
-  //   return new Promise((res) => {
-  //     res(this.mesh)
-  //   })
-  //
-  // }
-
-  // render(scene) {
-  //
-  //   return new Promise((res) => {
-  //     BABYLON.SceneLoader.ImportMesh(null, "/public/models/","Player1.stl", scene, (newMeshes, particleSystems, skeletons) => {
-  //       // this.model = newMeshes[0]
-  //       // this.mesh = BABYLON.MeshBuilder.CreateBox("box", {height: 10, width:5, depth:5}, scene);
-  //       // this.mesh.addChild(this.model);
-  //
-  //       console.log(newMeshes)
-  //       this.mesh = newMeshes[0]
-  //       this.mesh.ellipsoid = new BABYLON.Vector3(5, 20, 5);
-  //
-  //       // this.mesh.scaling = new BABYLON.Vector3(2, 2, 2);
-  //       // this.mesh.physicsImpostor = new BABYLON.PhysicsImpostor( this.mesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 100, restitution: 1, friction:1}, scene);
-  //       // // player.material.diffuseTexture  = new BABYLON.Texture( "/public/models/textures/lumberJack_diffuse.png",  scene);
-  //       // // // m.material.specularTexture = new BABYLON.Texture( "/file/specular.png", scene);
-  //       // // // m.material.ambientTexture  = new BABYLON.Texture( "/file/ambient.png",  scene);
-  //       // // // m.material.bumpTexture     = new BABYLON.Texture( "/file/bump.png",     scene);
-  //
-  //       this.mesh.position.y = 75
-  //       this.mesh.position.z = 150
-  //
-  //
-  //       res(this.mesh)
-  //     })
-  //   })
-  //
-  // }
 
   render(scene) {
     return new Promise((res) => {
-      BABYLON.SceneLoader.ImportMesh(null, "/public/models/dark_elf/", "Scene.gltf", scene, function (meshes, particleSystems, skeletons) {
-
-      });
+      BABYLON.SceneLoader.ImportMesh(null, "/public/models/male_adventurer/", "scene.gltf", scene, (meshes, particleSystems, skeletons) => {
+        this.mesh = meshes[0]
+        this.mesh.scaling = new BABYLON.Vector3(10, 10, -1);
+        this.mesh.position.y = 200
+        this.mesh.ellipsoid = new BABYLON.Vector3(8, 20, 2);
+        this.mesh.ellipsoidOffset = new BABYLON.Vector3(0, 8.0, 0);
+        // let material = new BABYLON.StandardMaterial('playerMaterial', scene);
+        // material.alpha = 1;
+        // material.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
+        // var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameterX:this.mesh.ellipsoid.x, diameterY: this.mesh.ellipsoid.y, diameterZ: this.mesh.ellipsoid.z}, scene);
+        // sphere.position = new BABYLON.Vector3(0, 200, 0);
+        // this.mesh.addChild(sphere)
+        // sphere.material = material
+        //if u want to draw ellipsoid to see it
+        res(this.mesh)
+      })
     })
   }
 
